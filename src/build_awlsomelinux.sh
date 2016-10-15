50e8eb19ccda13159f7b713c3b1c81956bb38e73
awlsomealex <awlsomeabc1@outlook.com>
2016-10-15 17:31:19 -0400

    Made AwlsomeLinux Builder less annoying.

--- src/build_awlsomelinux.sh
+++ src/build_awlsomelinux.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-echo "Welcome to AwlsomeLinux Builder!"
-sleep 5
-echo "This Script will automatically generate AwlsomeLinux."
-sleep 10
-clear
-echo "Checking Depenedencies (Apt)"
-sudo apt install wget make gawk gcc bc syslinux genisoimage g++ build-essential
-sleep 5
-echo "Lets Begin!"
-sleep 5
-clear
 
+echo "Checking Depenedencies (Apt)"
+sudo apt install wget make gawk gcc bc syslinux genisoimage g++ build-essential libncurses-dev
+sleep 5
 time sh 00_clean.sh
 time sh 01_kernel.sh
 time sh 02_glibc.sh
@@ -20,11 +12,6 @@ time sh 04_src.sh
 time sh 05_rootfs.sh
 time sh 06_syslinux.sh
 time sh 07_image.sh
-
 clear
 echo "AwlsomeLinux has now been built!"
-echo "Thank you for using AwlsomeLinux Builder!"
-sleep 5
-clear
-echo "AwlsomeLinux Created by AwlsomeAlex under GNU GPLv3"
-echo "Original Project based on Ivandavidov's Minimal Script"
+
