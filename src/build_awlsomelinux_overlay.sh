c278cdd9c672102f40215ad969ef31b808a33506
AwlsomeAlex <AwlsomeAlex@users.noreply.github.com>
2016-10-18 14:58:41 -0400

    Remove AwlsomeLinux Overlay Script

--- src/build_awlsomelinux_overlay.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-time sh overlay_clean.sh
-time sh overlay_glibc_full.sh
-time sh overlay_links.sh
-time sh overlay_dropbear.sh
-time sh overlay_ncurses.sh
-time sh overlay_nano.sh
-time sh overlay_mll_utils.sh
