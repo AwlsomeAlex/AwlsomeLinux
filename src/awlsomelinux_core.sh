79c422f6e04615c45afc028c55452aa77503f211
awlsomealex <awlsomeabc1@outlook.com>
2016-10-18 15:52:30 -0400

    Prepared Scripts

--- src/awlsomelinux_core.sh
+++ src/awlsomelinux_core.sh
@@ -1,3 +1,105 @@
 #!/bin/sh
 
 ## AwlsomeLinux Core Creation Script
+## Created by AwlsomeAlex (GNU GPLv3)
+
+##############################
+# -------------------------- #
+# AwlsomeLinux Configuration #
+# -------------------------- #
+##############################
+
+
+
+##########################
+# AwlsomeLinux Variables #
+##########################
+
+
+
+##############################
+# AwlsomeLinux Main Packages #
+##############################
+
+
+
+############################
+# ------------------------ #
+# AwlsomeLinux Core Script #
+# ------------------------ #
+############################
+
+
+
+##################
+# Kernel Compile #
+##################
+
+get_kernel() {
+	echo "Get Kernel"
+}
+
+build_kernel() {
+	echo "Build Kernel"
+}
+
+
+
+#################
+# Glibc Compile # 
+#################
+
+get_glibc() {
+	echo "Get Glibc"
+}
+
+build_glibc() {
+	echo "Build Glibc"
+}
+
+prepare_glibc() {
+	echo "Prepare Glibc"
+}
+
+
+
+###################
+# Busybox Compile #
+###################
+
+get_busybox() {
+	echo "Get Busybox"
+}
+
+build_busybox() {
+	echo "Build Busybox"
+}
+
+
+
+###################
+# RootFS Creation #
+###################
+
+prepare_src() {
+	echo "Prepare Source Code"
+}
+
+generate_rootfs() {
+	echo "Generate RootFS"
+}
+
+pack_rootfs() {
+	echo "Pack RootFS"
+}
+
+
+
+####################
+# Extract Syslinux #
+####################
+
+get_syslinux() {
+	echo "Get Syslinux"
+}
+
--- src/awlsomelinux_image.sh
+++ src/awlsomelinux_image.sh
@@ -1,3 +1,58 @@
 #!/bin/sh
 
 ## AwlsomeLinux Image Creation Script
+## Created by AwlsomeAlex (GNU GPLv3)
+
+##############################
+# -------------------------- #
+# AwlsomeLinux Configuration #
+# -------------------------- #
+##############################
+
+
+
+##########################
+# AwlsomeLinux Variables #
+##########################
+
+
+
+#############################
+# ------------------------- #
+# AwlsomeLinux Image Script #
+# ------------------------- #
+#############################
+
+
+
+###########################
+# Prepare Image Directory #
+###########################
+
+prepare_imagedir() {
+	echo "Prepare Image Directory"
+}
+
+
+
+#################################
+# Run OverlayFS Creation Script #
+#################################
+
+generate_overlayfs() {
+	echo "Generate OverlayFS"
+}
+
+
+
+###################################
+# Generate AwlsomeLinux ISO Image #
+###################################
+
+generate_image() {
+	echo "Generate AwlsomeLinux ISO Image"
+}
+
+generate_image
+generate_overlayfs
+prepare_imagedir
